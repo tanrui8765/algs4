fd5ef02ece2809c224462165583b335b8526dfcb
the_real_Rui <tanrui8765@hotmail.com>
2017-02-03 21:49:12 +0800

    exercises up to 1.3.2.

--- src/Exercises_1_3.java
+++ src/Exercises_1_3.java
@@ -5,6 +5,14 @@ public class Exercises_1_3
 {
 
 
+	public static void exercise_1_3_2(String[] args)
+	{
+		String input[] = {"it was - the best - of times - - - it was - the - -"};
+		Stack_TR<String> st = new Stack_TR<String>();
+
+		st.main(input);
+	}
+
 	public static void exercise_1_3_1(String[] args)
 	{
 		// exercise 1.3.1 is completed in the java file of FixedCapacityStackofStrings_TR
@@ -12,6 +20,6 @@ public class Exercises_1_3
 
 	public static void main(String[] args)
 	{
-		exercise_1_3_1(args);
+		exercise_1_3_2(args);
 	}
 }
---
+++ src/Stack_TR.java
@@ -0,0 +1,96 @@
+/**
+ * Created by the_real_Rui on 3/2/2017.
+ */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class Stack_TR<Item> implements Iterable<Item>
+{
+	private Node first; // top of stack (most recently added node)
+	private int N;
+
+	private class Node
+	{
+		// nested class to define nodes
+		Item item;
+		Node next;
+	}
+
+	public boolean isEmpty()
+	{
+		return first == null;
+	}
+
+	public int size()
+	{
+		return N;
+	}
+
+	public void push(Item item)
+	{
+		//Add item to top of stack.
+		Node oldfirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldfirst;
+		N++;
+	}
+
+	public Item pop()
+	{
+		// Remove item from top of stack
+		Item item = first.item;
+		first = first.next;
+		N--;
+		return item;
+	}
+
+	public Iterator<Item> iterator()
+	{
+		return new ListIterator();
+	}
+
+	private class ListIterator implements Iterator<Item>
+	{
+		private Node current = first;
+
+		public boolean hasNext()
+		{
+			return current != null;
+		}
+
+		public void remove()
+		{
+		}
+
+		public Item next()
+		{
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		// Create a stack and push/pop strings as directed on StdIn.
+		Stack_TR<String> s = new Stack_TR<String>();
+		String[] a = args[0].split(" ");
+
+//		while(!StdIn.isEmpty())
+		for (int i = 0; i < a.length; i++)
+		{
+//			String item = StdIn.readString();
+			String item = a[i];
+			if (!item.equals("-"))
+				s.push(item);
+			else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
+		}
+
+		StdOut.println("(" + s.size() + " left on stack)");
+	}
+}
