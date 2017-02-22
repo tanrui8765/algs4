deec99b4fadd58a086d1bb8349021b414be0cc9d
the_real_Rui <tanrui8765@hotmail.com>
2017-02-22 22:48:23 +0800

    exercises up to 1.3.34.

--- src/RandomBag_TR.java
+++ src/RandomBag_TR.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -13,7 +16,7 @@ public class RandomBag_TR<Item> implements Iterable<Item>
 
 	public RandomBag_TR()
 	{
-		a = (Item[]) new Object[2];
+		a = (Item[]) new Object[10];
 		n = 0;
 		first = 0;
 		last = 0;
@@ -37,6 +40,7 @@ public class RandomBag_TR<Item> implements Iterable<Item>
 		for (int i = 0; i < n; i++)
 		{
 			temp[i] = a[(first + i) % a.length];
+//			temp[i] = a[i];
 		}
 		a = temp;
 		first = 0;
@@ -60,6 +64,13 @@ public class RandomBag_TR<Item> implements Iterable<Item>
 		if (n == a.length) resize(2 * a.length);
 
 		a[last++] = item;
+
+//		StdOut.printf("array: ");
+//		for(int p=0;p<a.length;p++)
+//		{
+//			StdOut.printf("%5d, ",a[p]);
+//		}
+//		StdOut.println();
 		n++;
 	}
 
@@ -80,6 +91,35 @@ public class RandomBag_TR<Item> implements Iterable<Item>
 
 	private class ArrayIterator implements Iterator<Item>
 	{
+		public ArrayIterator()
+		{
+			shuffle(a);
+		}
+
+//		private void shuffle(Item b[])
+//		{
+//			int N = b.length;
+//			for(int j=0;j<N;j++)
+//			{
+//				int r = j+(int)(Math.random()*(N-j));
+//				Item temp = b[j];
+//				b[j] = b[r];
+//				b[r] = temp;
+//			}
+//		}
+
+		private void shuffle(Item b[])
+		{
+			int N = size();
+			for (int j = 0; j < N; j++)
+			{   // Exchange a[i] with random element in a[i..N-1]
+				int r = j + StdRandom.uniform(N - j);
+				Item temp = b[j];
+				b[j] = b[r];
+				b[r] = temp;
+			}
+		}
+
 		private int i = 0;
 
 		public boolean hasNext()
@@ -99,5 +139,33 @@ public class RandomBag_TR<Item> implements Iterable<Item>
 			i++;
 			return item;
 		}
+	}
+
+	/* Unit test helper */
+	private static void showList(RandomBag_TR list)
+	{
+		StdOut.println(list);
+		if (!list.isEmpty())
+		{
+			StdOut.printf("Size: %d, First: %s, Last: %s\n\n", list.size(), list.first(), list.last());
+		}
+		else
+		{
+			StdOut.printf("Size: %d\n\n", list.size());
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		StdOut.println("add:");
+		int[] q = {1, 3, 5, 7, 9};
+		RandomBag_TR<Integer> list = new RandomBag_TR<Integer>();
+
+		for (int i = 0; i < q.length; i++)
+		{
+			list.add(q[i]);
+		}
+//		showList(list);
+		StdOut.println(list.toString());
 	}
 }
