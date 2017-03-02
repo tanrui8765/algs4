80bbcb2de92187b48e7122a324062bb14514bca0
the_real_Rui <tanrui8765@hotmail.com>
2017-03-02 23:44:47 +0800

    exercise is up to 1.3.39, first created.

--- src/RingBuffer_TR.java
+++ src/RingBuffer_TR.java
@@ -1,6 +1,14 @@
 /**
  * Created by the_real_Rui on 2/3/2017.
  */
-public class RingBuffer_TR
+public class RingBuffer_TR<Item>
 {
+	final private int cq_size = 10;
+	private Item[] cq = (Item[]) new Object[cq_size];
+	private int first = 0;
+	private int last = 0;
+	private int n = 0;
+
+
+
 }
