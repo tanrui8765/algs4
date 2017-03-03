02f959a216ca36d6fa42082d85b5a10fbc9212fb
the_real_Rui <tanrui8765@hotmail.com>
2017-03-03 23:10:09 +0800

    exercise is up to 1.3.39, first created.

--- src/RingBuffer_TR.java
+++ src/RingBuffer_TR.java
@@ -1,7 +1,7 @@
 /**
  * Created by the_real_Rui on 2/3/2017.
  */
-public class RingBuffer_TR<Item>
+public class RingBuffer_TR<Item> implements Iterable<Item>
 {
 	final private int cq_size = 10;
 	private Item[] cq = (Item[]) new Object[cq_size];
@@ -9,6 +9,22 @@ public class RingBuffer_TR<Item>
 	private int last = 0;
 	private int n = 0;
 
+	// is the queue empty?
+	// return true if the queue is empty, otherwise false
+	public boolean isEmpty()
+	{
+		return n == 0;
+	}
 
+	// return number of items in this queue
+	public int size()
+	{
+		return n;
+	}
+
+	public void add(Item item)
+	{
+
+	}
 
 }
