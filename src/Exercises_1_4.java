450109e9cda1d4d6a2974d2e64020b0c1c9e96dc
the_real_Rui <tanrui8765@hotmail.com>
2017-03-22 23:27:46 +0800

    exercises 1.4.1 finished.

--- src/Exercises_1_4.java
+++ src/Exercises_1_4.java
@@ -4,7 +4,14 @@
  */
 public class Exercises_1_4
 {
-	// 19/3/2017 Reading.
-	// 20/3/2017 Reading Again.
-	// 21/3/2017 Reading.
+	public static void exercise_1_4_1(String[] args)
+	{
+		// this is an combination problem.
+		// chosen different triples from N items, the total number is C3/N.
+		// C3/N = P3/N / 3! = N! / (N-3)! / 3!
+		//      = N(N-1)(N-2)/6
+		//
+		// for fundamental permutation and combination explanation, please refer to
+		// "http://www.cnblogs.com/simbon/p/6074575.html"
+	}
 }
