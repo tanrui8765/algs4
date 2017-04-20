02324b6901ffcef3aa0642a5b7f6990690385df1
the_real_Rui <tanrui8765@hotmail.com>
2017-04-20 23:43:55 +0800

    exercise 1.4.19 to be finished.

---
+++ src/LocalMinMatrix_TR.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by the_real_Rui on 20/4/2017.
+ */
+public class LocalMinMatrix_TR
+{
+
+	public boolean isLocalMin(int[][] matrix, int x, int y)
+	{
+
+	}
+
+
+	public static void main(String[] args)
+	{
+		int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+		StdOut.println(a[2][2]);
+	}
+}
