9a2b13db184bb81375fbb195ac82b551e2d725b7
the_real_Rui <tanrui8765@hotmail.com>
2017-04-30 23:06:31 +0800

    exercise 1.4.19 to be finished.

--- src/LocalMinMatrix_TR2.java
+++ src/LocalMinMatrix_TR2.java
@@ -29,10 +29,178 @@ public class LocalMinMatrix_TR2
 		int j = (left + right) / 2;
 		int column_min = findColumnMin(matrix, j);
 
-		if ()
+		boolean is_local_min = isLocalMin(matrix, j, column_min);
+
+		if (is_local_min == false)
+		{
+
+		}
+		else
+		{
+
+		}
+
+	}
+
+	// Return
+	// -1: Not a conner point
+	// 0: left top point
+	// 1: right top point
+	// 2: right bottom point
+	// 3: left bottom point
+	public static int isConnerPoint(int[][] mat, int x, int y)
+	{
+		int ret_val = -1;
+
+		if ((x == 0) && (y == 0))
+		{
+			ret_val = 0;
+		}
+		else if ((x == mat[0].length - 1) && (y == 0))
+		{
+			ret_val = 1;
+		}
+		else if ((x == mat[0].length - 1) && (y == mat.length - 1))
+		{
+			ret_val = 2;
+		}
+		else if ((x == 0) && (y == mat.length - 1))
+		{
+			ret_val = 3;
+		}
+
+		return ret_val;
+	}
+
+	// Return
+	// -1: Not a side point
+	// 0: top side
+	// 1: right side
+	// 2: bottom side
+	// 3: left side
+	public static int isSidePoint(int[][] mat, int x, int y)
+	{
+		int ret_val = -1;
+
+		if (((x > 0) && (x < mat[0].length - 1)) && (y == 0))
+		{
+			ret_val = 0;
+		}
+		else if ((x == mat[0].length - 1) && ((y > 0) && (y < mat.length - 1)))
+		{
+			ret_val = 1;
+		}
+		else if (((x > 0) && (x < mat[0].length - 1)) && (y == mat.length - 1))
+		{
+			ret_val = 2;
+		}
+		else if ((x == 0) && ((y > 0) && (y < mat.length - 1)))
+		{
+			ret_val = 3;
+		}
+
+		return ret_val;
+	}
 
 
+	public static boolean isLocalMin(int[][] matrix, int x, int y)
+	{
+		boolean is_local_min = false;
+		int is_conner_point = isConnerPoint(matrix, x, y);
+		if (is_conner_point == -1)
+		{
+			int is_side_point = isSidePoint(matrix, x, y);
+			if (is_side_point == -1)
+			{
+				// Normal Points
+				if ((matrix[x][y] < matrix[x - 1][y]) &&
+						(matrix[x][y] < matrix[x + 1][y]) &&
+						(matrix[x][y] < matrix[x][y - 1]) &&
+						(matrix[x][y] < matrix[x][y + 1]))
+				{
+					is_local_min = true;
+				}
 
+			}
+			else
+			{
+				// Side Points
+				if (is_side_point == 0)      // top side
+				{
+					if ((matrix[x][y] < matrix[x - 1][y]) &&
+							(matrix[x][y] < matrix[x + 1][y]) &&
+							(matrix[x][y] < matrix[x][y + 1]))
+					{
+						is_local_min = true;
+					}
+				}
+				else if (is_side_point == 1) // right side
+				{
+					if ((matrix[x][y] < matrix[x - 1][y]) &&
+							(matrix[x][y] < matrix[x][y - 1]) &&
+							(matrix[x][y] < matrix[x][y + 1]))
+					{
+						is_local_min = true;
+					}
+				}
+				else if (is_side_point == 2) // bottom side
+				{
+					if ((matrix[x][y] < matrix[x - 1][y]) &&
+							(matrix[x][y] < matrix[x + 1][y]) &&
+							(matrix[x][y] < matrix[x][y - 1]))
+					{
+						is_local_min = true;
+					}
+				}
+				else if (is_side_point == 3) // left side
+				{
+					if ((matrix[x][y] < matrix[x + 1][y]) &&
+							(matrix[x][y] < matrix[x][y - 1]) &&
+							(matrix[x][y] < matrix[x][y + 1]))
+					{
+						is_local_min = true;
+					}
+				}
+			}
+		}
+		else
+		{
+			// Conner Points
+			if (is_conner_point == 0)        // left top
+			{
+				if ((matrix[x][y] < matrix[x + 1][y]) &&
+						(matrix[x][y] < matrix[x][y + 1]))
+				{
+					is_local_min = true;
+				}
+			}
+			else if (is_conner_point == 1)   // right top
+			{
+				if ((matrix[x][y] < matrix[x - 1][y]) &&
+						(matrix[x][y] < matrix[x][y + 1]))
+				{
+					is_local_min = true;
+				}
+			}
+			else if (is_conner_point == 2)   // right bottom
+			{
+				if ((matrix[x][y] < matrix[x - 1][y]) &&
+						(matrix[x][y] < matrix[x][y - 1]))
+				{
+					is_local_min = true;
+				}
+			}
+			else if (is_conner_point == 3)   // left bottom
+			{
+				if ((matrix[x][y] < matrix[x + 1][y]) &&
+						(matrix[x][y] < matrix[x][y - 1]))
+				{
+					is_local_min = true;
+				}
+			}
+		}
+
+		return is_local_min;
 	}
 
 	public static void main(String[] args)
