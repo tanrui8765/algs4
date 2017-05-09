fc0ab80dfaf1376c3d0c5aea0bae4ec67f21e50e
the_real_Rui <tanrui8765@hotmail.com>
2017-05-09 23:43:09 +0800

    exercise 1.4.22 not finished.

--- src/FibonacciSearch_TR.java
+++ src/FibonacciSearch_TR.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * Created by the_real_Rui on 8/5/2017.
  */
@@ -75,8 +77,58 @@ public class FibonacciSearch_TR
 		return -1;
 	}
 
+	int fbsearch(int[] arry, int target)
+	{
+		int fm1 = 1;
+		int fm2 = 0;
+		int fm0 = fm1 + fm2;
+
+		while (fm0 < arry.length)
+		{
+			fm2 = fm1;
+			fm1 = fm0;
+			fm0 = fm1 + fm2;
+		}
+
+		int offset = -1;
+		while (fm1 > 0)
+		{
+			int mid = min((offset + fm1), arry.length - 1);
+			if (arry[mid] == target)
+				return mid;
+			else if (arry[mid] < target)
+			{
+				fm0 = fm2;
+				fm1 = fm1 - fm2;
+				fm2 = fm2 - fm1;
+				offset = mid;
+			}
+			else
+			{
+				fm0 = fm1;
+				fm1 = fm2;
+				fm2 = fm0 - fm1;
+			}
+		}
+		return -1;
+	}
+
 	public static void main(String[] args)
 	{
+		int array[] = new int[10];
+		for (int i = 0; i < 10; i++)
+		{
+			array[i] = i + 8;
+			StdOut.print(array[i] + " ");
+		}
+		StdOut.println();
 
+		FibonacciSearch_TR fs = new FibonacciSearch_TR();
+		for (int i : array)
+		{
+			StdOut.print("i is " + i + "; ");
+			StdOut.println(fs.fbsearch(array, i));
+//			StdOut.println(fs.search(array,i,array.length));
+		}
 	}
 }
