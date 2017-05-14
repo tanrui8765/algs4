1cec4e34c0588d42c0f1a7b69005b05c7e542788
the_real_Rui <tanrui8765@hotmail.com>
2017-05-14 18:36:37 +0800

    Job Stuff, centering.

--- src/DecoCentering_TR.java
+++ src/DecoCentering_TR.java
@@ -25,17 +25,20 @@ public class DecoCentering_TR
 		cent_points = new Point2D[sample_num];
 		code_points = new Point2D[sample_num];
 
-		cent_points[0] = new Point2D(20, 20);
-		cent_points[1] = new Point2D(60, 20);
-		cent_points[2] = new Point2D(60, 40);
-		cent_points[3] = new Point2D(20, 40);
-//		cent_points[4] = new Point2D(20,20);
+		cent_points[0] = new Point2D(480, 400);
+		cent_points[1] = new Point2D(660, 400);
+		cent_points[2] = new Point2D(660, 480);
+		cent_points[3] = new Point2D(480, 480);
+		cent_points[4] = new Point2D(((cent_points[0].x() + cent_points[2].x()) / 2 + (cent_points[1].x() + cent_points[3].x()) / 2) / 2,
+				((cent_points[0].y() + cent_points[2].y()) / 2 + (cent_points[1].y() + cent_points[3].y()) / 2) / 2);
 
 		code_points[0] = new Point2D(200, 20);
 		code_points[1] = new Point2D(600, 20);
 		code_points[2] = new Point2D(600, 40);
 		code_points[3] = new Point2D(200, 40);
-//		code_points[4] = new Point2D(200,20);
+		code_points[4] = new Point2D(((code_points[0].x() + code_points[2].x()) / 2 + (code_points[1].x() + code_points[3].x()) / 2) / 2,
+				((code_points[0].y() + code_points[2].y()) / 2 + (code_points[1].y() + code_points[3].y()) / 2) / 2);
+
 	}
 
 	public void drawRegions()
@@ -52,6 +55,17 @@ public class DecoCentering_TR
 		StdDraw.line(cent_points[0].x(), cent_points[0].y(), cent_points[3].x(), cent_points[3].y());
 		StdDraw.line(code_points[0].x(), code_points[0].y(), code_points[3].x(), code_points[3].y());
 
+
+		StdDraw.setPenColor(StdDraw.BLUE);
+		for (int i = 0; i < sample_num; i++)
+		{
+			StdDraw.filledCircle(cent_points[i].x(), cent_points[i].y(), 2);
+		}
+		StdDraw.setPenColor(StdDraw.MAGENTA);
+		for (int i = 0; i < sample_num; i++)
+		{
+			StdDraw.filledCircle(code_points[i].x(), code_points[i].y(), 2);
+		}
 
 	}
 
