305cdbca0fca245e8ab74c81829108868e56c9ce
the_real_Rui <tanrui8765@hotmail.com>
2017-06-09 23:34:07 +0800

    exercise 1.4.27 to be finished.

--- src/QueueWithTwoStacks_TR.java
+++ src/QueueWithTwoStacks_TR.java
@@ -1,8 +1,18 @@
 /**
  * Created by the_real_Rui on 7/6/2017.
  */
-public class QueueWithTwoStacks_TR
+public class QueueWithTwoStacks_TR<Item>
 {
 	// A queue is first in first out
 	// A queue is last in first out
+
+	public void enqueue(Item item)
+	{
+
+	}
+
+	public Item dequeue()
+	{
+
+	}
 }
