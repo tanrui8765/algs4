2ef442986b5cee85e202ff4ea2dea754f17e2308
the_real_Rui <tanrui8765@hotmail.com>
2017-06-12 07:21:16 +0800

    exercise 1.4.29 to be finished.

--- src/StequeWithTwoStacks_TR.java
+++ src/StequeWithTwoStacks_TR.java
@@ -1,8 +1,49 @@
+import edu.princeton.cs.algs4.Stack;
+
 /**
  * Created by the_real_Rui on 11/6/2017.
  */
 public class StequeWithTwoStacks_TR<Item>
 {
+	private Stack<Item> in_stk;
+	private Stack<Item> out_stk;
+	private int n_nodes = 0;
 
+	public StequeWithTwoStacks_TR()
+	{
+		in_stk = new Stack<Item>();
+		out_stk = new Stack<Item>();
+		n_nodes = 0;
+	}
+
+	public int size()
+	{
+		return n_nodes;
+	}
+
+	public boolean isEmpty()
+	{
+		return (n_nodes == 0);
+	}
+
+	public void push(Item item)
+	{
+
+	}
+
+	public Item pop()
+	{
+
+	}
+
+	public void enqueue(Item item)
+	{
+
+	}
+
+	public Item dequeue()
+	{
+
+	}
 
 }
