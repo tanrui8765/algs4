cf82309242a029fa58118b16cab8130f1241cd11
the_real_Rui <tanrui8765@hotmail.com>
2017-06-19 22:09:13 +0800

    exercise 1.4.31 to be finished.

--- src/DequeWithThreeStacks_TR.java
+++ src/DequeWithThreeStacks_TR.java
@@ -1,9 +1,17 @@
+import edu.princeton.cs.algs4.Stack;
+
 /**
  * Created by the_real_Rui on 18/6/2017.
  */
 public class DequeWithThreeStacks_TR<Item>
 {
+	private Stack<Item> stack1, stack2, stack3;
+	private int n_nodes = 0;
 
+	public DequeWithThreeStacks_TR()
+	{
+
+	}
 
 	public void main(String[] args)
 	{
