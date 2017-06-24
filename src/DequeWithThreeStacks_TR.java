fecda8e6446ee101e6f5a20982de3a14c899dd40
the_real_Rui <tanrui8765@hotmail.com>
2017-06-24 23:10:45 +0800

    exercise 1.4.31 to be finished.

--- src/DequeWithThreeStacks_TR.java
+++ src/DequeWithThreeStacks_TR.java
@@ -2,6 +2,8 @@ import edu.princeton.cs.algs4.Stack;
 
 /**
  * Created by the_real_Rui on 18/6/2017.
+ *
+ * https://github.com/zalacer/projects-tn/blob/862c13adc573d6bd88ab242790f67eb5bebc243f/Algorithms4edCh1%2B2/src/ds/DequeWithThreeStacks.java
  */
 public class DequeWithThreeStacks_TR<Item>
 {
