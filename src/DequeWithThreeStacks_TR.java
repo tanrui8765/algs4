5cfc2cabf678553a8f2ce455b9b64215d37bb497
the_real_Rui <tanrui8765@hotmail.com>
2017-07-11 23:06:19 +0800

    exercise 1.4.31 to be finished.

--- src/DequeWithThreeStacks_TR.java
+++ src/DequeWithThreeStacks_TR.java
@@ -48,67 +48,11 @@ public class DequeWithThreeStacks_TR<Item>
 	//Amortized O(1)
 	public Item popLeft()
 	{
-		// remove and return an item from the left end, i.e. the first item.
-		// rebalance the allocation of items between stack left and stack right if appropriate
-
-		if (isEmpty()) throw new NoSuchElementException("DequeWithThreeStacks underflow");
-		if (!stack_left.isEmpty())
-		{
-			return stack_left.pop();
-		}
-		else
-		{
-			int s = stack_right.size();
-			if (s == 1) return stack_right.pop();
-			if (s - 1 > 1) // balance stack_left and stack_right (excuse to use stack_mid)
-			{
-				for (int i = 0; i < (s - 1) / 2; i++)
-				{
-					stack_mid.push(stack_right.pop());
-				}
-				s = stack_right.size();
-				for (int i = 0; i < s - 1; i++)
-				{
-					stack_left.push(stack_right.pop());
-				}
-				Item first = stack_right.pop();
-				s = stack_mid.size();
-				for (int i = 0; i < s; i++)
-				{
-					stack_right.push(stack_mid.pop());
-				}
-				return first;
-			}
-			else
-			{
-				for (int i = 0; i < s - 1; i++)
-				{
-					stack_left.push(stack_right.pop());
-				}
-				return stack_right.pop();
-			}
-		}
 	}
 
 	//Amortized O(1)
 	public Item popRight()
 	{
-		// remove and return an item from the right end, i.e. the last item
-		// rebalance the allocation of items between stack1 and stack2 if appropriate
-		if (isEmpty()) throw new NoSuchElementException("DequeWithThreeStacks underflow");
-
-		if (!stack_right.isEmpty())
-			return stack_right.pop();
-		else
-		{
-			int s = stack_left.size();
-			if (s == 1) return stack_left.pop();
-			if (s - 1 > 1) // balance stack_left and stack_right (excuse to use stack_mid)
-			{
-				for (int i = 0; i < (s - 1) / 2; i++) stack_right.push(stack_left.pop());
-			}
-		}
-
 	}
 
 	public void main(String[] args)
