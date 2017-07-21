5aedf6d09056ca81b16a39f638186952b8edd509
the_real_Rui <tanrui8765@hotmail.com>
2017-07-21 23:34:31 +0800

    exercise 1.4.34 to be finished.

--- src/Exercises_1_4.java
+++ src/Exercises_1_4.java
@@ -334,6 +334,11 @@ public class Exercises_1_4
 		// https://github.com/zalacer/projects-tn/blob/862c13adc573d6bd88ab242790f67eb5bebc243f/Algorithms4edCh1%2B2/src/ex14/Ex1433MemoryRequirementsOn32BitMachine.java
 	}
 
+	public static void exercise_1_4_34(String[] args)
+	{
+
+	}
+
 	public static void main(String[] args)
 	{
 		exercise_1_4_12(args);
