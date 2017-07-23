e26e42b4773b2662a60dc965e7cb9e23b646c966
the_real_Rui <tanrui8765@hotmail.com>
2017-07-23 23:53:00 +0800

    exercise 1.4.34 to be finished.

--- src/HotorCold_TR.java
+++ src/HotorCold_TR.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 /**
@@ -15,7 +16,7 @@ public class HotorCold_TR
 		return StdRandom.uniform(min_num, (max_num + 1));
 	}
 
-	public static int guess_once(int min_num, int max_num)
+	public static int new_guess(int min_num, int max_num, int prev_guess_value, int prev_guess_rate)
 	{
 
 	}
@@ -26,7 +27,7 @@ public class HotorCold_TR
 	}
 
 	// return result: guess doesn't go further or closer 0; closer 1; further -1;
-	public static int guess_result_dir(int secret_num, int guess_num, int prev_guess_num)
+	public static int guess_result_rating(int secret_num, int guess_num, int prev_guess_num)
 	{
 		int ret_val = 0;
 
@@ -50,8 +51,49 @@ public class HotorCold_TR
 		return ret_val;
 	}
 
-	public static int guess_secret_num(int secret_num)
+	public static void guess_progress_trace(int guess_index, boolean is_rlt_hit, int new_val, int new_rate, int prev_val, int prev_rate)
 	{
+		StdOut.println(guess_index + ", "
+				+ is_rlt_hit + ", "
+				+ new_val + ", "
+				+ new_rate + ", "
+				+ prev_val + ", "
+				+ prev_rate);
+	}
+
+	public static void guess_secret_num(int secret_num, int min_num, int max_num)
+	{
+		boolean is_secret_num_found = false; // this value mean is guess hit the secret number?
+
+		int new_guess_val = 0;
+		int prev_guess_val = 0;
+
+		int new_guess_rate = 0;
+		int prev_guess_rate = 0;
+
+		int guess_cnt = 0;
+
+		do
+		{
+			guess_cnt++;
+
+			new_guess_val = new_guess(min_num, max_num, prev_guess_val, prev_guess_rate);
+			is_secret_num_found = is_guess_hit(secret_num, new_guess_val);
+			new_guess_rate = guess_result_rating(secret_num, new_guess_val, prev_guess_val);
+
+			if (is_secret_num_found == false)
+			{
+				prev_guess_val = new_guess_val;
+				prev_guess_rate = new_guess_rate;
+			}
+			else
+			{
+				;
+			}
+
+			guess_progress_trace(guess_cnt, is_secret_num_found, new_guess_val, new_guess_rate, prev_guess_val, prev_guess_rate);
+		}
+		while (is_secret_num_found == false);
 
 	}
 
@@ -60,7 +102,10 @@ public class HotorCold_TR
 		int N = 100;
 		int secret_num = 0;
 
+
 		secret_num = generate_a_secret_num(1, N);
+
+		guess_secret_num(secret_num, 1, N);
 
 
 	}
